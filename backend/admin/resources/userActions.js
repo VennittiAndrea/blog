e079f4fe524e523b5a0bbd0f3f26cbb9abd29ee4
Andrea Vennitti <andrea.vennitti@gmail.com>
2022-04-02 13:01:12 +0200

    Add also validation of AdminJs

--- backend/admin/resources/userActions.js
+++ backend/admin/resources/userActions.js
@@ -1,30 +1,55 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const { ValidationError } = require("adminjs");
 
 //solo l'admin può modificare i ruoli degli utenti
-const canModifyUsers = ({ currentAdmin }) => currentAdmin && currentAdmin.role === 'admin';
+const canModifyUsers = ({ currentAdmin }) =>
+  currentAdmin && currentAdmin.role === "admin";
 
-const elementAction= {
-    edit: { isAccessible: canModifyUsers },
-    delete: { isAccessible: canModifyUsers },
-    new: {
-        isAccessible: canModifyUsers,
-        before: async (request) => {
-            if(request.payload.password) {
-            request.payload = {
-                ...request.payload,
-                encryptedPassword: await bcrypt.hash(request.payload.password, 10),
-                password: undefined,
-            }
-            }
-            return request;
-        },
-        after: async(response) => {
-            if(response.record && response.record.errors){
-                response.record.errors.password= response.record.errors.encryptedPassword;
-            }
-            return response;
-        },
+const elementAction = {
+  edit: { isAccessible: canModifyUsers },
+  delete: { isAccessible: canModifyUsers },
+  new: {
+    isAccessible: canModifyUsers,
+    before: async (request) => {
+      if (!validator.isEmail(request.payload.email)) {
+        throw new ValidationError({
+            email: {
+              message: 'E-Mail is invalid.',
+            },
+          }, {
+            message: 'something wrong happened',
+          });
+      }
+      if (
+        validator.isEmpty(request.payload.password) ||
+        !validator.isLength(request.payload.password, { min: 5 })
+      ) {
+        throw new ValidationError({
+            password: {
+              message: 'Password is invalid. At least 5 characters are required.',
+            },
+          }, {
+            message: 'something wrong happened',
+          });
+      }
+      if (request.payload.password) {
+        request.payload = {
+          ...request.payload,
+          encryptedPassword: await bcrypt.hash(request.payload.password, 10),
+          password: undefined,
+        };
+      }
+      return request;
     },
-}
+    after: async (response) => {
+      if (response.record && response.record.errors) {
+        response.record.errors.password =
+          response.record.errors.encryptedPassword;
+      }
+      return response;
+    },
+  },
+};
 
 module.exports = elementAction;
